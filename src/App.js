00e4c10016994242f5813265bdcfb99baaf98e7d
Aliia <malaevaid@gmail.com>
2023-09-30 11:53:49 +0600

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Routing from "./Routing";
 import Navbar from "./components/Navbar/Navabr";
-import HomePage from "./pages/HomePage/HomePage";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <HomePage />
-      {/* <Routing /> */}
+      <Routing />
     </>
   );
 };
