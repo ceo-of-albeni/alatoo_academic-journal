11efc4a76f6777148dc443d415a32fb62d5db032
Aliia <malaevaid@gmail.com>
2023-10-04 21:00:54 +0600

    renewed homepage and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Routing from "./Routing";
 import Navbar from "./components/Navbar/Navabr";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routing />
+      <Footer />
     </>
   );
 };
