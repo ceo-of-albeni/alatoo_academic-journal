66fad65fa464898b5a73e7b923e1fbd3a1d5358f
Aliia <malaevaid@gmail.com>
2023-10-23 23:32:44 +0600

    allat2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Footer.scss";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
 
 const Footer = () => {
   return (
     <footer>
-      <p>© 2023, Name</p>
+      <p>© 2023, Copyright</p>
+      <div className="footer_icon">
+        <InstagramIcon />
+        <TelegramIcon />
+      </div>
     </footer>
   );
 };
