92b1e7d581cfa33e4a1739ef5347435d589a599a
Kurosu9 <kurstan.abazbekov@alatoo.edu.kg>
2024-01-28 17:00:17 +0600

    update_modals

--- src/components/Navbar/modals/forgotPassword/ForgotPassword.js
+++ src/components/Navbar/modals/forgotPassword/ForgotPassword.js
@@ -2,14 +2,9 @@ import React, { useState, useContext, useEffect } from "react";
 import classes from "./ForgotPassword.module.css";
 import { useNavigate } from "react-router-dom";
 import arrow from "../img/arrow.svg";
-import { Login } from "../login/Login";
-import { ChangePassword } from "../changePassword/ChangePassword";
 import { authContext } from "../../../../contexts/authContext";
 
 export function ForgotPassword({ closeModal }) {
-  const [modalOpen, setModalOpen] = useState(true);
-  const [openLogin, setOpenLogin] = useState(false);
-  const [openChange, setOpenChange] = useState(false);
   const [email, setEmail] = useState("");
 
   const { forgotPassword, setError } = useContext(authContext);
@@ -25,11 +20,12 @@ export function ForgotPassword({ closeModal }) {
     };
 
     forgotPassword(obj);
-    closeOpenChange();
 
     console.log(email);
 
     setEmail("");
+
+    navigate("/change_password");
   }
 
   useEffect(() => {
@@ -38,56 +34,32 @@ export function ForgotPassword({ closeModal }) {
 
   const navigate = useNavigate();
 
-  const handleForgotClick = e => {
-    e.stopPropagation();
-  };
-
-  const handleOutsideClick = () => {
-    closeModal();
-  };
-
-  const closeOpenLogin = () => {
-    setModalOpen(false);
-    setOpenLogin(true);
-  };
-
-  const closeOpenChange = e => {
-    // e.preventDefault();
-    setModalOpen(false);
-    setOpenChange(true);
-  };
 
   return (
-    <>
-      {modalOpen && (
-        <div className={classes.forgot} onClick={handleOutsideClick}>
-          <div className={classes.forgot__inner} onClick={handleForgotClick}>
-            <img src={arrow} alt="back" onClick={closeOpenLogin} />
-            <form action="">
-              <div>Password Reset</div>
-              <div className={classes.text}>
-                <p>
-                  Forgotten your password? Enter your e-mail address below, and
-                  we'll send you an e-mail allowing you to reset it.
-                </p>
-              </div>
-              <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                placeholder="Enter your email"
-                // value={email}
-                // onChange={e => setEmail(e.target.value)}
-                name="email"
-              />
-              <button onClick={handleData}>Reset my password</button>
-            </form>
+    <div className={classes.forgot}>
+      <div className={classes.forgot__inner}>
+        <img src={arrow} alt="back" onClick={() => navigate("/")} />
+        <form action="">
+          <div>Password Reset</div>
+          <div className={classes.text}>
+            <p>
+              Forgotten your password? Enter your e-mail address below, and
+              we'll send you an e-mail allowing you to reset it.
+            </p>
           </div>
-        </div>
-      )}
-      {openLogin && <Login closeModal={setOpenLogin} />}
-      {openChange && <ChangePassword closeModal={setOpenChange} />}
-    </>
+          <label>Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            // value={email}
+            // onChange={e => setEmail(e.target.value)}
+            name="email"
+          />
+          <button onClick={handleData}>Reset my password</button>
+        </form>
+      </div>
+    </div>
   );
 }
