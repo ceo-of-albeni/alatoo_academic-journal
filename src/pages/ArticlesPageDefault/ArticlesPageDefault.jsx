b23a33ab6c36f8c9ee295a3537a8f3ac5de1488b
aliia <aliia.malaeva@alatoo.edu.kg>
2025-04-04 03:52:08 +0600

    search for articles and Download PDF

--- src/pages/ArticlesPageDefault/ArticlesPageDefault.jsx
+++ src/pages/ArticlesPageDefault/ArticlesPageDefault.jsx
@@ -9,20 +9,10 @@ import ArticleCardDefault from "../../components/ArticleCardDefault/ArticlesCard
 const ArticlesPageDefault = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [search, setSearch] = useState(searchParams.get("q") || "");
-
   const [page, setPage] = useState(1);
   const { t, i18n } = useTranslation();
 
-  //const { id } = useParams();
   const { approved_articles, getAllApproved } = useContext(articlesContext);
-
-  // useEffect(() => {
-    //getAllApproved();
- // }, []);
-
-  useEffect(() => {
-    getAllApproved();
-  }, [searchParams]);
 
   useEffect(() => {
     setSearchParams({
@@ -30,11 +20,16 @@ const ArticlesPageDefault = () => {
     });
   }, [search]);
 
+  useEffect(() => {
+    getAllApproved();
+  }, [searchParams]);
 
-  //console.log(approved_articles);
+  const filteredArticles = approved_articles.filter((article) =>
+    article.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   const itemsOnPage = 8;
-  const count = Math.ceil(approved_articles.length / itemsOnPage);
+  const count = Math.ceil(filteredArticles.length / itemsOnPage);
 
   const handlePage = (e, p) => {
     setPage(p);
@@ -43,43 +38,41 @@ const ArticlesPageDefault = () => {
   const currentData = () => {
     const begin = (page - 1) * itemsOnPage;
     const end = begin + itemsOnPage;
-    return approved_articles.slice(begin, end);
+    return filteredArticles.slice(begin, end);
   };
 
   return (
     <div className={classes.list_main}>
       <div className={classes.list_main_div}>
         <h1>{t("approved_articles.title")}</h1>
-        <div className={classes.list_courses_div}>
-          
         <div className={classes.filAndSr}>
-        <a href="#">
-          <div className={classes.icon}>
-            <i className="fa-solid fa-filter fa-2xl"></i>
-          </div>
-        </a>
-        <a href="#">
-          <div className={classes.icon} id={classes.search}>
-            <i
-              className="fa-solid fa-magnifying-glass fa-2xl"
-              id={classes.search}></i>
-          </div>
-        </a>
-        <input
-          type="search"
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-          placeholder="Search..."
-          className={classes.sr_inp}
-        />
-      </div>
-
-          {approved_articles ? (
+          <a href="#">
+            <div className={classes.icon}>
+              <i className="fa-solid fa-filter fa-2xl"></i>
+            </div>
+          </a>
+          <a href="#">
+            <div className={classes.icon} id={classes.search}>
+              <i
+                className="fa-solid fa-magnifying-glass fa-2xl"
+                id={classes.search}></i>
+            </div>
+          </a>
+          <input
+            type="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search..."
+            className={classes.sr_inp}
+          />
+        </div>
+        <div className={classes.list_courses_div}>
+          {filteredArticles.length > 0 ? (
             currentData().map((item) => (
               <ArticleCardDefault key={item.id} item={item} />
             ))
           ) : (
-            <h3>Loading...</h3>
+            <h3>No results found...</h3>
           )}
         </div>
 
